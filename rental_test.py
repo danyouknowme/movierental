52e337442f345194a1c44c60a9b2bc08e4aabbe3
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-17 01:17:04 +0700

    add test for rental points function

--- rental_test.py
+++ rental_test.py
@@ -17,7 +17,6 @@ class RentalTest(unittest.TestCase):
 		self.assertEqual("CitizenFour", m.get_title())
 		self.assertEqual(Movie.REGULAR, m.get_price_code())
 
-	# @unittest.skip("TODO add this test when you refactor rental price")
 	def test_rental_price(self):
 		rental = Rental(self.new_movie, 1)
 		self.assertEqual(rental.get_charge(), 3.0)
@@ -32,6 +31,17 @@ class RentalTest(unittest.TestCase):
 		rental = Rental(self.childrens_movie, 7)
 		self.assertEqual(rental.get_charge(), 7.5)
 
-	@unittest.skip("TODO add test of frequent renter points when you add it to Rental")
 	def test_rental_points(self):
-		self.fail("TODO add  test of frequent renter points")
+		rental = Rental(self.new_movie, 1)
+		self.assertEqual(rental.get_frequent_renter_points(), 1)
+		rental = Rental(self.new_movie, 4)
+		self.assertEqual(rental.get_frequent_renter_points() , 4)
+		rental = Rental(self.regular_movie, 1)
+		self.assertEqual(rental.get_frequent_renter_points() , 1)
+		rental = Rental(self.regular_movie, 8)
+		self.assertEqual(rental.get_frequent_renter_points() , 1)
+		rental = Rental(self.childrens_movie, 1)
+		self.assertEqual(rental.get_frequent_renter_points() , 1)
+		rental = Rental(self.childrens_movie, 16)
+		self.assertEqual(rental.get_frequent_renter_points() , 1)
+			
