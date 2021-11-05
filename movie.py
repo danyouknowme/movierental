3ed2159258675964d911887073e913bdd0a9b0ae
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-11-05 20:48:07 +0700

    remove price code from movie class

--- movie.py
+++ movie.py
@@ -3,19 +3,9 @@ from rental import PriceCode
 class Movie:
     """A movie available for rent."""
 
-    # The types of movies (price_code).
-    REGULAR = PriceCode.regular
-    NEW_RELEASE = PriceCode.new_release
-    CHILDRENS = PriceCode.children
-
-    def __init__(self, title, price_code):
+    def __init__(self, title):
         # Initialize a new movie.
         self.title = title
-        self.price_code = price_code
-
-    def get_price_code(self):
-        # get the price code
-        return self.price_code
 
     def get_title(self):
         return self.title
